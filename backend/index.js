f02c2b292a6a255f31ba0badf311557760e8d04a
Hari-Bombon <haripriya14022003@gmail.com>
2023-04-08 21:01:28 +0530

    login form

---
+++ backend/index.js
@@ -0,0 +1,60 @@
+const express = require("express");
+const cors = require("cors");
+const mongoose = require("mongoose");
+
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+mongoose.connect(
+    "mongodb+srv://admin:admin@cluster0.gq2a9v4.mongodb.net/Kavach",
+  () => {
+    console.log("connected to DB");
+  }
+);
+
+const userSchema = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+const User = mongoose.model("User", userSchema);
+
+app.post("/Login", (req, res) => {
+  const { email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      if (password === user.password) {
+        res.send({ message: "login success", user: user });
+      } else {
+        res.send({ message: "wrong credentials" });
+      }
+    } else {
+      res.send({ message: "not registered" });
+    }
+  });
+});
+
+app.post("/Register", (req, res) => {
+  const { name, email, password } = req.body;
+  User.findOne({ email: email }, (err, user) => {
+    if (user) {
+      res.send({ message: "user already exists" });
+    } else {
+      const newUser = new User({ name, email, password });
+      newUser.save((err) => {
+        if (err) {
+          res.send(err);
+        } else {
+          res.send({ message: "successfully registered" });
+        }
+      });
+    }
+  });
+});
+
+app.listen(6969, () => {
+  console.log("server started");
+});
--- src/pages/components/Register.tsx
+++ src/pages/components/Register.tsx
@@ -1,8 +1,11 @@
 import React, { useState } from 'react'
 import axios from "axios";
-
+import 'react-toastify/dist/ReactToastify.css';
+import { toast } from 'react-toastify';
 
 function Register() {
+    const notify = () => toast("Wow You're registered!");
+
     const [user,setUser] = useState({
         name:"",
         email:"",
@@ -20,25 +23,46 @@ function Register() {
     e.preventDefault();
    const {name,email,password} = user
    if (name && email && password){
-    // axios.post("http://localhost:6969/Register",user )
-    // .then(res=>console.log(res))
+    axios.post("http://localhost:6969/Register",user )
+    .then(res=>{
+        if (res.status === 200){
+        toast.success("Success!");
+        }else {
+            toast.error("Invalid data field")
+        }                 
+        console.log(res)
+    })                
     console.log(user)
    }
    else{
-       alert("invalid input")
+
+    toast.error('🦄 type all data fields!', {
+        position: "top-right",
+        autoClose: 5000,
+        hideProgressBar: false,
+        closeOnClick: true,
+        pauseOnHover: true,
+        draggable: true,
+        progress: undefined,
+        theme: "light",
+        });
+ 
+       
    }
 }
    
   return (
     <div>
 
-<div className="flex flex-col max-w-md px-4 py-8 bg-white rounded-lg shadow dark:bg-gray-800 sm:px-6 md:px-8 lg:px-10">
-    <div className="self-center mb-2 text-xl font-light text-gray-800 sm:text-2xl dark:text-white">
+<div className="flex flex-col max-w-md px-10 py-10 bg-black rounded-lg shadow dark:bg-cyan-300 sm:px-6 md:px-10 lg:px-18">
+    <div className="self-center mb-2 text-xl font-light text-cyan-400	color: rgb(34 211 238);
+Aa sm:text-2xl dark:text-white">
         Create a new account
     </div>
-    <span className="justify-center text-sm text-center text-gray-500 flex-items-center dark:text-gray-400">
+    <span className="justify-center text-sm text-center text-cyan-400	color: rgb(34 211 238);
+Aa flex-items-center dark:text-gray-400">
         Already have an account ?
-        <a href="#" target="_blank" className="text-sm text-blue-500 underline hover:text-blue-700">
+        <a href="#" target="_blank" className="px-2 text-sm text-red-500 underline hover:text-blue-700">
             Sign in
         </a>
     </span>
@@ -46,22 +70,22 @@ function Register() {
         <form action="#">
             <div className="flex flex-col mb-2">
                 <div className=" relative ">
-                    <input type="text" id="create-account-pseudo" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="name" value={user.name} onChange={handleChange}   placeholder="FullName"/>
+                    <input type="text" id="create-account-pseudo" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-blue text-gray-700 placeholder-black-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-white-600 focus:border-transparent" name="name" value={user.name} onChange={handleChange}   placeholder="FullName"/>
                     </div>
                 </div>
                 <div className="flex gap-4 mb-2">
                     <div className=" relative ">
-                        <input type="text" id="create-account-first-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="email" value={user.email} onChange={handleChange}   placeholder="Email"/>
+                        <input type="email" id="create-account-first-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-blue-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-white-600 focus:border-transparent" name="email" value={user.email} onChange={handleChange}   placeholder="Email"/>
                         </div>
 
                         </div>
                         <div className="flex flex-col mb-2">
                             <div className=" relative ">
-                                <input type="password" id="create-account-email" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" name="password" value={user.password} onChange={handleChange}      placeholder="password"/>
+                                <input type="password" id="create-account-email" className=" rounded-lg border-transparent flex-1 appearance-none border border-blue-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-white-400 focus:border-transparent" name="password" value={user.password} onChange={handleChange}      placeholder="password"/>
                                 </div>
                             </div>
                             <div className="flex w-full my-4">
-                                <button type="submit" className="py-2 px-4  bg-purple-600 hover:bg-purple-700 focus:ring-purple-500 focus:ring-offset-purple-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" onClick={regist} >
+                                <button type="submit" className="py-2 px-4  bg-cyan-500 hover:bg-cyan-600 focus:ring-white-500 focus:ring-offset-blue-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg" onClick={regist} >
                                     Register
                                 </button>
                             </div>
